d03edc6f59886a4f90657a2a60bd65006d1f5c79
Sergio Lucero <sergiolucero73@gmail.com>
2022-06-07 23:48:59 -0300

    feat: Servicio de autenticacion añadido

--- src/__tests__/register.test.ts
+++ src/__tests__/register.test.ts
@@ -1,10 +1,17 @@
 import supertest from 'supertest'
 import server from '../index'
+import { store } from '../auth/signup.service'
 
 const request = supertest(server)
 
+jest.mock('../auth/signup.service')
+
+beforeEach(() => {
+  store.mockReset()
+})
+
 describe('POST in api/signup', () => {
-  it.skip('deberia registrar un usuario', async () => {    
+  it.skip('deberia registrar un usuario', async () => {
     const response = await request.post('/api/signup').send({
       email: 'username@mail.com',
       password: 'pass%2022',
@@ -15,8 +22,8 @@ describe('POST in api/signup', () => {
 
   it.skip('deberia devolver 400 si el email no esta presente', async () => {
     const response = await request.post('/api/signup').send({
-        password: 'pass%2022',
-        passwordConfirmation: 'pass%2022',
+      password: 'pass%2022',
+      passwordConfirmation: 'pass%2022',
     })
 
     expect(response.status).toBe(400)
@@ -27,7 +34,7 @@ describe('POST in api/signup', () => {
     const response = await request.post('/api/signup').send({
       email: 'username@mail.com',
       passwordConfirmation: 'pass%2022',
-    })    
+    })
     expect(response.status).toBe(400)
     expect(response.body.message).toBe('"password" es un dato requerido')
   })
@@ -36,12 +43,12 @@ describe('POST in api/signup', () => {
     const response = await request.post('/api/signup').send({
       email: 'username@mail.com',
       password: 'pass%2022',
-    })    
+    })
     expect(response.status).toBe(400)
     expect(response.body.message).toBe('"passwordConfirmation" es un dato requerido')
   })
 
-  it('deberia cevolver 400 si el password y la confirmacion del password no son iguales', async () => {
+  it.skip('deberia devolver 400 si el password y la confirmacion del password no son iguales', async () => {
     const response = await request.post('/api/signup').send({
       email: 'username@mail.com',
       password: 'pass%2021',
@@ -50,5 +57,17 @@ describe('POST in api/signup', () => {
     expect(response.status).toBe(400)
     expect(response.body.message).toBe('"password" y "passwordConfirmation" deben ser iguales')
   })
-})
 
+  it.skip('deberia devolver 400 si el campo del email no es una direccion de correo valida', async () => {
+    const response = await request.post('/api/signup').send({
+      email: 'usernamemail.com',
+      password: 'pass%2022',
+      passwordConfirmation: 'pass%2022',
+    })
+    expect(response.status).toBe(400)
+  })
+
+  it('deberia devolver 403 si el mail provisto ya se encuenta en uso', async () => {
+    // Mock de la base de datos
+  })
+})
--- src/auth/signup.controller.ts
+++ src/auth/signup.controller.ts
@@ -1,26 +1,35 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express'
 export const signupController = (request: Request, response: Response) => {
-    const {email, password, passwordConfirmation} = request.body
+  const { email, password, passwordConfirmation } = request.body
 
-    if (!email) {   
-        response.status(400).json({
-            message: '"email" es un dato requerido'
-        })
-    }
-    if (!password) {
-        response.status(400).json({
-            message: '"password" es un dato requerido'
-        })
-    }
-    if (!passwordConfirmation) {
-        response.status(400).json({
-            message: '"passwordConfirmation" es un dato requerido'
-        })
-    }
-    if(password !== passwordConfirmation) {
-        response.status(400).json({
-            message: '"password" y "passwordConfirmation" deben ser iguales'
-        })
-    }
-    response.status(201).send({})
+  if (!email) {
+    response.status(400).json({
+      message: '"email" es un dato requerido',
+    })
+  }
+  if (!password) {
+    response.status(400).json({
+      message: '"password" es un dato requerido',
+    })
+  }
+  if (!passwordConfirmation) {
+    response.status(400).json({
+      message: '"passwordConfirmation" es un dato requerido',
+    })
+  }
+  if (password !== passwordConfirmation) {
+    response.status(400).json({
+      message: '"password" y "passwordConfirmation" deben ser iguales',
+    })
+  }
+  if (
+    email.match(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    ) === null
+  ) {
+    response.status(400).json({
+      message: '"email" no es una direccion de correo valida',
+    })
+  }
+  response.status(201).send({})
 }
